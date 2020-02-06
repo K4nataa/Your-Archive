a439ce8e45f8a5f6fbd3fd01016364514159fd53
Joshua Otter <joshuaotter@gmail.com>
2020-02-06 13:15:32 +0100

    user en wat documents

---
+++ Your_Archive/Models/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Your_Archive.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public ApplicationUser() : base()
+        {
+
+        }
+
+        public string Gender { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+       
+        public int Phonenumber { get; set; }
+        
+        public string Emailadres { get; set; }
+        public string Wachtwoord { get; set; }
+        
+    }
+}
+
